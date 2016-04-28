2626ab8201893eab9bb78bae84d63e940fbda6a3
spwahaha <spwahaha@gmail.com>
2016-04-28 18:21:28 -0400

    change arguments

--- Project2/src/blocked/BlockedPageRank.java
+++ Project2/src/blocked/BlockedPageRank.java
@@ -20,19 +20,19 @@ public class BlockedPageRank {
 		for(i = 0; i < iterationNum; i++){
 			residuals = 0;
 			Configuration conf = new Configuration();
-			File[] files = new File("input/").listFiles();
-			String inputPath = null;
-			if(i == 0){
-				for(File file : files){
-					inputPath = file.getPath();
-					System.out.println("Use this input file: " + inputPath);
-					break;
-				}
-			}else{
-				inputPath = "stage" + (i - 1);
-			}
+//			File[] files = new File("input/").listFiles();
+//			if(i == 0){
+//				for(File file : files){
+//					inputPath = file.getPath();
+//					System.out.println("Use this input file: " + inputPath);
+//					break;
+//				}
+//			}else{
+//				inputPath = "stage" + (i - 1);
+//			}
+			String inputPath = i == 0 ? args[0] : args[1] + "/stage" + (i - 1);
 			
-			String outputPath = "stage" + i;
+			String outputPath = args[1] + "/stage" + i;
 			
 			Job job = new Job(conf, "Simple Page Rank");
 			
